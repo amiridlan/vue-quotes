ee4f89cb5b2f7c88e3b74c2ac177d47e3af8972e
amiridlan <53267532+amiridlan@users.noreply.github.com>
2025-03-19 10:16:48 +0800

    edited all

---
+++ src/components/RandomQuoteGenerator.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { onMounted, ref } from 'vue';
+import quotes from './quotes.js'; // Importing the quotes array
+
+const currentQuote = ref({ text: '', author: '' });
+
+const getRandomQuote = () => {
+  const randomIndex = Math.floor(Math.random() * quotes.length);
+  currentQuote.value = quotes[randomIndex];
+};
+
+onMounted(getRandomQuote);
+</script>
+
+<template>
+  <div
+    class="bg-gradient-to-tl from-lime-400 to-cyan-500 min-h-screen flex items-center justify-center"
+  >
+    <div class="bg-black max-w-xl mx-auto p-6 text-center border rounded-lg shadow-lg">
+      <h1 class="text-2xl mb-5 text-white">Random Quote Generator</h1>
+      <blockquote class="bg-gray-100 p-5 rounded-lg border border-gray-300 mb-5">
+        <p class="text-lg mb-2">{{ currentQuote.text }}</p>
+        <cite class="font-normal text-gray-500">{{ currentQuote.author }}</cite>
+      </blockquote>
+
+      <button
+        @click="getRandomQuote"
+        class="px-4 py-2 text-lg bg-lime-500 text-black rounded hover:bg-lime-700 hover:text-white"
+      >
+        New Quote
+      </button>
+    </div>
+  </div>
+</template>
---
+++ src/components/quotes.js
@@ -0,0 +1,56 @@
+const quotes = [
+  {
+    text: `Hard work betrays none, but dreams betray many.`,
+    author: `Hikigaya Hachiman`,
+  },
+  {
+    text: `I won't ever be the best at anything. Or rather, you might say that I've stopped trying to be.`,
+    author: `Fukube Satoshi`,
+  },
+  {
+    text: `If you can never get angry at anything, that probably means you have nothing that you like, either.`,
+    author: `Chitanda Eru`,
+  },
+  {
+    text: `I find it’s actually easier to be misunderstood. People’s stares stop bothering you, you’re severed from love. Turns out you find freedom when you stick out`,
+    author: `Keyakizaka46  - Eccentric`,
+  },
+  {
+    text: `The more inexperienced you are, the more you want to show off.`,
+    author: `Houtarou Oreki`,
+  },
+  {
+    text: `A joke only lasts for a moment. If it leaves a misunderstanding, it becomes a lie.`,
+    author: `Fukube Satoshi`,
+  },
+  {
+    text: `Fake people have an image to maintain, real people just don't care.`,
+    author: `Hikigaya Hachiman`,
+  },
+  {
+    text: `Everyone lies, everyone hides things. Nobody makes it through this life being completely honest.`,
+    author: `Orihara Izaya`,
+  },
+  {
+    text: `You’re happy enough, so don’t take away the happiness of others. You should share it.`,
+    author: `Simon Brezhnev`,
+  },
+  {
+    text: `I'm not against socializing, so long as it's not tiring.`,
+    author: `Houtarou Oreki`,
+  },
+  {
+    text: `If you don't like where you're at now, complain to your past self about it.`,
+    author: `Orihara Izaya`,
+  },
+  {
+    text: `I often wonder about that. Do our tears do any good for the dead?`,
+    author: `Kunikida Doppo`,
+  },
+  {
+    text: `There are those who actually think that powers can be a tool to seduce women.`,
+    author: `Sakaguchi Ango`,
+  },
+];
+
+export default quotes;
